cf9c29b56c786efd5055bc471d9d603db8162b18
Derek Stucki <derek.stucki@inl.gov>
2018-11-11 09:51:45 -0700

    basic parallel pi

--- pi.c
+++ pi.c
@@ -17,24 +17,37 @@ History: Written by Tim Mattson, 11/99.
 #include <omp.h>
 static long num_steps = 100000000;
 double step;
+static int max_threads = 6;
 int main ()
 {
-	  int i;
-	  double x, pi, sum = 0.0;
-	  double start_time, run_time;
+    int i;
+    double pi = 0.0;
+    double start_time, run_time;
 
-	  step = 1.0/(double) num_steps;
+    step = 1.0/(double) num_steps;
 
-	  start_time = omp_get_wtime();
+    start_time = omp_get_wtime();
 
-	  for (i=1;i<= num_steps; i++){
-		  x = (i-0.5)*step;
-		  sum = sum + 4.0/(1.0+x*x);
-	  }
+    omp_set_num_threads(max_threads);
+    double sums[max_threads];
+#pragma omp parallel
+    {
+        int num_threads = omp_get_num_threads();
+        int steps_per_thread = num_steps / num_threads;
+        int ID = omp_get_thread_num();
+        double sum = 0.0;
+	for (i=ID * steps_per_thread + 1;i <= (ID + 1) * steps_per_thread; i++){
+	    double x = (i-0.5)*step;
+            sum = sum + 4.0/(1.0+x*x);
+	}
+        sums[ID] = sum;
 
-	  pi = step * sum;
-	  run_time = omp_get_wtime() - start_time;
-	  printf("pi with %ld steps is %lf in %lf seconds\n",num_steps,pi,run_time);
+    }
+    for (i=0;i<max_threads;i++){
+        pi += step * sums[i];
+    }
+    run_time = omp_get_wtime() - start_time;
+    printf("pi with %ld steps is %lf in %lf seconds\n",num_steps,pi,run_time);
 }
 
 
